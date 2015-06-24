d3fdad68f5aebe302da3ca50862f8227f830dbfb
tugberkugurlu <tugberkugurlu@gmail.com>
2015-06-24 22:22:24 +0100

    added SelectRandomProverb sproc

---
+++ db/Stored Procedures/dbo.SelectRandomProverb.sql
@@ -0,0 +1,15 @@
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+
+CREATE PROCEDURE [dbo].[SelectRandomProverb]
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT TOP 1 Id, Content, IsVisible, CreatedOn
+	FROM dbo.Proverbs 
+	ORDER BY RAND();
+END
+GO
