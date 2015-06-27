7159a5da93afec0d5a5244d9da23475e767d9d35
tugberkugurlu <tugberkugurlu@gmail.com>
2015-06-27 04:26:47 +0100

    hooked db to app

--- src/BritishProverbs.Web/Controllers/HomeController.cs
+++ src/BritishProverbs.Web/Controllers/HomeController.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BritishProverbs.Web.Models;
 
 namespace BritishProverbs.Web.Controllers
 {
@@ -10,7 +14,54 @@ namespace BritishProverbs.Web.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            var proverbModel = GetRandomProverb();
+            return View(proverbModel);
+        }
+
+        private ProverbModel GetRandomProverb()
+        {
+            const string selectStatement = "EXEC dbo.SelectRandomProverb";
+            var connectionString = ConfigurationManager.ConnectionStrings["ConnStr"].ConnectionString;
+
+            using (var conn = new SqlConnection(connectionString))
+            {
+                using (var cmd = new SqlCommand())
+                {
+                    cmd.Connection = conn;
+                    cmd.CommandText = selectStatement;
+                    cmd.CommandType = CommandType.Text;
+
+                    conn.Open();
+
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        return reader.Select(r => ProverbModelBuilder(r)).First();
+                    }
+                }
+            }
+        }
+
+        private ProverbModel ProverbModelBuilder(SqlDataReader reader)
+        {
+            return new ProverbModel
+            {
+                Id = int.Parse(reader["Id"].ToString()),
+                Content = reader["Content"] is DBNull ? null : reader["Content"].ToString()
+            };
+        }
+    }
+
+    public static class Extensions
+    {
+
+        public static IEnumerable<T> Select<T>(
+            this SqlDataReader reader, Func<SqlDataReader, T> projection)
+        {
+
+            while (reader.Read())
+            {
+                yield return projection(reader);
+            }
         }
     }
 }
