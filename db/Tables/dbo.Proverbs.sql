68209c08f6bfa86f4356129be3acb076228eb92c
Tugberk Ugurlu <tugberk@outlook.com>
2015-09-30 15:16:34 +0100

    added last updated

--- db/Tables/dbo.Proverbs.sql
+++ db/Tables/dbo.Proverbs.sql
@@ -4,7 +4,8 @@ CREATE TABLE [dbo].[Proverbs]
 [UserId] [int] NOT NULL,
 [Content] [nvarchar] (2000) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 [IsVisible] [bit] NOT NULL,
-[CreatedOn] [datetimeoffset] NOT NULL
+[CreatedOn] [datetimeoffset] NOT NULL,
+[LastUpdatedOn] [datetimeoffset] NULL
 ) ON [PRIMARY]
 ALTER TABLE [dbo].[Proverbs] ADD 
 CONSTRAINT [PK_Proverbs] PRIMARY KEY CLUSTERED  ([Id]) ON [PRIMARY]
