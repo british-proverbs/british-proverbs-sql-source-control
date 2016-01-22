972c652950bb673ca7740cb8eccdb529e814f9f4
Tugberk Ugurlu <tugberk@outlook.com>
2016-01-22 11:36:51 +0000

    fii#

--- db/Tables/dbo.Visits.sql
+++ db/Tables/dbo.Visits.sql
@@ -1,7 +1,8 @@
 CREATE TABLE [dbo].[Visits]
 (
 [Id] [int] NOT NULL IDENTITY(1, 1),
-[CreatedOn] [datetimeoffset] NOT NULL
+[CreatedOn] [datetimeoffset] NOT NULL,
+[Foo] [nchar] (10) COLLATE Latin1_General_CI_AS NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[Visits] ADD CONSTRAINT [PK_Visits] PRIMARY KEY CLUSTERED  ([Id]) ON [PRIMARY]
